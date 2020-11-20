503f9fd465400ab52b6cc674dad859d7f5554097
Peter shaan <peterhiku12@gmail.com>
2020-11-20 09:31:46 +0700

    migartion database

---
+++ Week 3/Database Migration/database/migrations/2020_11_20_022924_create_komentar_pertanyaan_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarPertanyaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('komentar_pertanyaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('isi');
+            $table->unsignedBigInteger('pertanyaan_id')->nullable();
+            $table->unsignedBigInteger('profiles_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('pertanyaan_id')->references('id')->on('jawaban');
+            $table->foreign('profiles_id')->references('id')->on('profiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('komentar_pertanyaan');
+    }
+}
